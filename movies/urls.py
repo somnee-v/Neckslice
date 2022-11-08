1bbb2dc87383af8a4130b6c0e9fdfbc7b9d3f576
som2145 <som2145@gmail.com>
2022-11-08 12:41:34 +0900

    merge

--- movies/urls.py
+++ movies/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.movie_list),                  #영화 전체 리스트
     path('1/<int:movie_pk>/', views.movie_detail1),  # 영화 디테일 페이지 방법 1
-    path('2/<int:movie_pk>/',views.movie_detail2),   # 영화 디테일 페이지 방법 2
+    path('/<int:movie_pk>/',views.movie_detail2),   # 영화 디테일 페이지 방법 2
     path('average7/', views.vote_average7),        # 영화 평점 7점 이상 리스트
     path('genre/<genre_name>/',views.genre_list),         # 영화 장르 별 검색.
 
