7c0995e2ff21f09ffdfeec5fa39f7fabba9ef042
timeiscoal <timeiscoal@gmail.com>
2022-11-08 11:17:58 +0900

    리드미 최종 및 urls 정리

--- Neckslice/settings.py
+++ Neckslice/settings.py
@@ -30,13 +30,12 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'django_filters',
     'users',
-<<<<<<< HEAD
+
 
     #cors 설정
     'corsheaders',
 
-=======
->>>>>>> 8cfab5b2160f75dbaa4be34c00ea81ada38e4a17
+
     # 22.11.06 최신욱 추가.
     'movies.apps.MoviesConfig',
 ]
